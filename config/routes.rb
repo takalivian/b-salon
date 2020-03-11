c826501f5abce380b0c95ad4b27752a0e74847c4
takalivian <61145524+takalivian@users.noreply.github.com>
2020-03-11 16:22:13 +0900

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, :only => [:index, :show]
+  root "users#index"
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
+end
