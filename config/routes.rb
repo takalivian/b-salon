d53fee2de94a47078d434d7f24da6e7a87131206
takalivian <61145524+takalivian@users.noreply.github.com>
2020-03-11 16:52:33 +0900

    reset scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, :only => [:index, :show]
-  root "users#index"
-  resources :messages, :only => [:create]
-  resources :rooms, :only => [:create, :show, :index]
+  root to:"posts#index"
+  resources :users, only: [:index, :show]
+
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :show, :index]
+  resources :posts, only: [:index]
 end
