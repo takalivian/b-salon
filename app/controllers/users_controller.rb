d1ca96f11e94966a66f1f64318872fb5b7ab181e
takalivian <61145524+takalivian@users.noreply.github.com>
2020-03-16 16:47:41 +0900

    リロード画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @users=User.all
     @name = current_user.name
     # @posts = current_user.posts
     @user=User.find(params[:id])
