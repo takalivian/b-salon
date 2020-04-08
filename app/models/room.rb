18e506d75ce6cb14a3843478443779dcc1a36043
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-08 09:58:21 +0900

    ラストメッセージ、画像表示

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,15 +2,4 @@ class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
 
-  def show_last_message
-    if (last_message = messages.last).present?
-      if last_message.content?
-        last_message.content
-      else
-        '画像が投稿されています'
-      end
-    else
-      'まだメッセージはありません。'
-    end
-  end
 end
