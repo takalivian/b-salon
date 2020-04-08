505cb8ede1281e997a5d9aa0ff5afad8ebc36da8
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-08 19:50:50 +0900

    PostValidation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, :date, :area_id, :postal, :place, :venue, :capacity, :remarks, presence: true
 
   def self.search(search)
     return Post.all unless search
