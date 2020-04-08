38ce443bc06fd6005daf044ee652151b963914f0
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-08 18:41:04 +0900

    routeリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"users#index"
   resources :users, only: [:index, :show, :edit, :update]
-
   resources :messages, only: [:index, :create]
   resources :rooms, only: [:create, :show, :index]
-  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+  resources :posts do
     collection do
       get 'search'
     end
