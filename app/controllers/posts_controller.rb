12812c908f0fed0cfd92477f5f612c4f194c32a0
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-11 01:48:10 +0900

    投稿新着順

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :setting_post, only: [:update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
