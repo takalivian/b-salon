d732156d4f6a42164d7ec067dc097c585115d229
takalivian <61145524+takalivian@users.noreply.github.com>
2020-04-12 02:35:00 +0900

    UserFlash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      redirect_to user_path
+      redirect_to user_path, notice: '更新しました'
     else
       render :edit
     end
